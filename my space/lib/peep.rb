c114bd5431624568166d08878e2e4963848fbffb
Risha <rishallen@gmail.com>
2016-04-08 13:51:59 -0700

    more work

---
+++ my space/lib/peep.rb
@@ -0,0 +1,9 @@
+# class Peep
+#   attr_reader :username, :favorite_animal
+#   def initialize(options = {})
+#     @name = options["username"]
+#     @fave_animal = options["favorite_animal"]
+#   end
+# end
+
+# peep = Peep.new(params["peep"])
---
+++ my space/myapp.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require_relative 'lib/peep'
+
+class MyApp < Sinatra::Base
+
+
+get '/home' do
+  @images_one = %w(skull.png ) # singleflower.png seaanimal.jpg
+  @images_two = %w(blk_spots.png ) # palmtrees.png beach.jpg
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/code_journal' do
+  erb :code_journal
+end
+
+get '/portfolio' do
+  erb :portfolio
+end
+
+  run!
+end
